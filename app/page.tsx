a887d74830d124ad7577b4625feceed6f0689cae
Aditya Kumar <aditya1756in@gmail.com>
2025-06-03 13:58:16 -0600

    Implementing the Click mE

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+'use client';
 import Image from "next/image";
+import { use, useState } from "react";
 
 export default function Home() {
+  const [num, setNum] = useState(0);
+
+  const incNum = () => {
+    setNum(num + 1);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <>
+      <button type="button" onClick={incNum} className="btn btn-primary btn-lg">Click Me: {num}</button>
+    </>
+  );
+}
+
+{/*
+  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      
+    </div>
+  <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -97,7 +114,4 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
-    </div>
-  );
-}
+      </footer> */}
